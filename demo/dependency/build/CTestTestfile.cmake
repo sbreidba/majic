140548f1d9d777be04bbb84bf6f98f22875aeb84
Skip Breidbach <sbreidbach@sabfusevm.(none)>
2013-09-30 17:44:21 -0700

    Remove build files

--- demo/dependency/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/hgfs/Devel/clavin/dependency
-# Build directory: /mnt/hgfs/Devel/clavin/dependency/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(build/exports/googletest-0.0.2-SNAPSHOT)
-SUBDIRS(test)
--- demo/dependency/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/hgfs/Devel/clavin/dependency/test
-# Build directory: /mnt/hgfs/Devel/clavin/dependency/build/test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(maintest "artidepTest")
