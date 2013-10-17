67b2ed57bc16787df3b1f6ef8d4909221d2f4b60
Philip Miller <philip.miller@sri.com>
2013-10-17 17:10:52 -0400

    avoid separate html doc installation step by building html output directly to html folder rather than to intermediate _build folder

--- doc/src/make-html.sh
+++ doc/src/make-html.sh
@@ -1,4 +1,4 @@
 set -x
 export src_dir=../..
 export build_dir=../_build
-sphinx-build -c . -d ${build_dir} -b html ${src_dir} ${build_dir}/html
+sphinx-build -c . -d ${build_dir} -b html ${src_dir} ../html
