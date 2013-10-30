d6c3595a275afa1eeffd57f501242371dc1be8fc
Philip Miller <philip.miller@sri.com>
2013-10-30 02:27:02 -0400

    move config.py and index.rst from doc/src/site/sphinx to top-level so that we will get the conventional benefits of sphinx for documenting source code

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ release = '0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'README.rst']
+exclude_patterns = ['_build', 'README.rst', 'doc/target']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
@@ -117,7 +117,7 @@ html_theme = 'sphinxdoc' # 'default'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['doc/src/site/sphinx/_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
---
+++ index.rst
@@ -0,0 +1,33 @@
+.. top-level TOC file for clavin
+
+================================
+Welcome to clavin documentation!
+================================
+
+A pdf version is available :download:`here <doc/src/site/sphinx/clavin.pdf>`
+
+.. toctree::
+   :maxdepth: 2
+
+   doc/src/site/sphinx/gettingStarted
+   doc/src/site/sphinx/clavin
+   doc/src/site/sphinx/configureGit
+   doc/src/site/sphinx/jenkinsSetup
+   doc/src/site/sphinx/configureUbuntu
+   doc/src/site/sphinx/configureCentos
+   doc/src/site/sphinx/configureVc10
+   doc/src/site/sphinx/linuxNotes
+
+   doc/src/site/sphinx/todoList
+
+==================
+Indices and tables
+==================
+
+* :ref:`genindex`
+
+.. * :ref:`modindex`
+
+* :ref:`search`
+
+
