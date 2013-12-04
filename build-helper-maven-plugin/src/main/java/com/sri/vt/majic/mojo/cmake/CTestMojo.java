d8220b7d920016e5dbd89214dd983f1559a9a73e
Skip <skip.breidbach@sri.com>
2013-12-04 10:26:03 -0800

    [MAJIC-51] Create test goal

---
+++ build-helper-maven-plugin/src/main/java/com/sri/vt/majic/mojo/cmake/CTestMojo.java
@@ -0,0 +1,75 @@
+package com.sri.vt.majic.mojo.cmake;
+
+import com.sri.vt.majic.mojo.ExecMojo;
+import com.sri.vt.majic.util.CMakeDirectories;
+import org.apache.commons.lang3.SystemUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@Mojo(name="cmake-test", defaultPhase= LifecyclePhase.TEST, requiresProject=true)
+public class CTestMojo extends CMakeMojo
+{
+    @Parameter(alias = "executable", defaultValue = "ctest")
+    private String exeName;
+
+    @Parameter(defaultValue = "true")
+    boolean verbose;
+
+    @Parameter(defaultValue = "skipTests")
+    boolean skip;
+
+    @Override
+    protected boolean getSkip()
+    {
+        if (super.getSkip() || skip) return true;
+
+        File testConfigFile = new File(getWorkingDirectory(), "CTestTestfile.cmake");
+        if (!testConfigFile.exists())
+        {
+            getLog().info("No test config file (" + testConfigFile.getAbsolutePath() + ") - skipping tests");
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected String getCommandlineArgs()
+    {
+        StringBuilder builder = new StringBuilder();
+
+        String config = getCurrentConfig();
+        if ((config != null) && (config.length() != 0))
+        {
+            builder.append(" --build-config ");
+            builder.append(config);
+        }
+
+        if (verbose)
+        {
+            builder.append(" --verbose");
+        }
+
+        return builder.toString();
+    }
+
+    @Override
+    protected String getExecutable()
+    {
+        return exeName;
+    }
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        execute(ExecutionMode.ExecutionPerConfig);
+    }
+}
