fc82f1c59def7c8cbb57858918ef193f2d57a631
Skip Breidbach <skip.breidbach@sri.com>
2013-12-19 14:17:32 -0800

    [MAJIC-27] Add import/export artifacts mojos - needed to create a manifest of what was attached when operating in jenkins multi-os mode

---
+++ build-helper-maven-plugin/src/main/java/com/sri/vt/majic/mojo/ExportArtifactsMojo.java
@@ -0,0 +1,114 @@
+package com.sri.vt.majic.mojo;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.wagon.PathUtils;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomWriter;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Mojo(name="export-artifacts", defaultPhase= LifecyclePhase.VERIFY)
+public class ExportArtifactsMojo extends AbstractMojo
+{
+    @Parameter(defaultValue = "false", property = "exportArtifacts.skip")
+    boolean skip;
+    
+    @Parameter(defaultValue = "false", property = "exportArtifacts.includeSelf")
+    boolean includeSelf;
+
+    @Parameter(defaultValue = "artifacts.xml", property = "exportArtifacts.outputFileName")
+    String outputFileName;
+
+    @Parameter(defaultValue = "${project.build.directory}", property = "exportArtifacts.outputDirectory")
+    File outputDirectory;
+    
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    MavenProject project;
+
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        if (skip)
+        {
+            getLog().info("Skipping execution");
+            return;
+        }
+
+        Collection<Artifact> artifacts = new ArrayList<Artifact>();
+
+        if (includeSelf)
+        {
+            artifacts.add(project.getArtifact());
+        }
+
+        artifacts.addAll(project.getAttachedArtifacts());
+
+        Writer writer;
+        try
+        {
+            File outputFile = new File(outputDirectory, outputFileName);
+            writer = new PrintWriter(outputFile, "UTF-8");
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new MojoExecutionException("Could not create output file", e);
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            throw new MojoExecutionException("Could not create output file: Unsupported Encoding: ", e);
+        }
+
+        Xpp3Dom dom = new Xpp3Dom("artifacts");
+        for (Artifact artifact : artifacts)
+        {
+            getLog().debug("Found artifact " + artifact);
+            Xpp3Dom xmlArtifact = new Xpp3Dom("artifact");
+
+            {
+                Xpp3Dom xmlType = new Xpp3Dom("type");
+                xmlType.setValue(artifact.getType());
+                xmlArtifact.addChild(xmlType);
+            }
+            
+            {
+                Xpp3Dom xmlClassifier = new Xpp3Dom("classifier");
+                xmlClassifier.setValue(artifact.getClassifier());
+                xmlArtifact.addChild(xmlClassifier);
+            }
+
+            {
+                Xpp3Dom xmlFile = new Xpp3Dom("file");
+                if (artifact.getFile() != null)
+                {
+                    xmlFile.setValue(PathUtils.toRelative(outputDirectory, artifact.getFile().getAbsolutePath()));
+                    xmlArtifact.addChild(xmlFile);
+                }
+                else
+                {
+                    getLog().error("Null artifact file: " + artifact);
+                }
+            }
+
+            dom.addChild(xmlArtifact);
+        }
+
+        Xpp3DomWriter.write(writer, dom);
+        
+        try
+        {
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException("Could not close output file", e);
+        }
+    }
+}
---
+++ build-helper-maven-plugin/src/main/java/com/sri/vt/majic/mojo/ImportArtifactsMojo.java
@@ -0,0 +1,120 @@
+package com.sri.vt.majic.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
+import org.codehaus.plexus.util.xml.Xpp3Dom;
+import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.*;
+
+@Mojo(name="import-artifacts")
+public class ImportArtifactsMojo extends AbstractMojo
+{
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    MavenProject project;
+
+    @Parameter(defaultValue = "artifacts.xml", property = "importArtifacts.inputFileName")
+    String inputFileName;
+
+    @Parameter(defaultValue = "${project.build.directory}", property = "importArtifacts.inputDirectory")
+    File inputDirectory;
+
+    @Parameter(defaultValue = "true", property = "importArtifacts.recurse")
+    boolean recurse;
+
+    @Component()
+    private MavenProjectHelper projectHelper;
+
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+        if (!recurse)
+        {
+            importArtifacts(inputDirectory);
+        }
+        else
+        {
+            recursiveImport(inputDirectory);
+        }
+    }
+
+    public void recursiveImport(File directory) throws MojoExecutionException
+    {
+        for (File subFile : directory.listFiles())
+        {
+            File artifactFile = new File(subFile, inputFileName);
+            if (artifactFile.exists())
+            {
+                getLog().info("importing from " + artifactFile);
+                importArtifacts(subFile);
+            }
+            
+            if (subFile.isDirectory())
+            {
+                recursiveImport(subFile);
+            }
+        }
+    }
+
+    public void importArtifacts(File directory) throws MojoExecutionException
+    {
+        Reader reader;
+        try
+        {
+            File inputFile = new File(directory, inputFileName);
+            FileInputStream inputStream = new FileInputStream(inputFile);
+            reader = new InputStreamReader(inputStream, "UTF-8");
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new MojoExecutionException("Could not open file", e);
+        }
+        catch (UnsupportedEncodingException e)
+        {
+            throw new MojoExecutionException("Could not open file: Unsupported Encoding: ", e);
+        }
+
+        Xpp3Dom dom = null;
+        try
+        {
+            dom = Xpp3DomBuilder.build(reader);
+        }
+        catch (XmlPullParserException e)
+        {
+            throw new MojoExecutionException("Error reading file: ", e);
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException("Error reading file: ", e);
+        }
+
+        for (Xpp3Dom artifact : dom.getChildren())
+        {
+            String type = artifact.getChild("type").getValue();
+            String classifier = artifact.getChild("classifier").getValue();
+            String file = artifact.getChild("file").getValue();
+
+            File artifactFile = new File(directory, file);
+            if (!artifactFile.exists())
+            {
+                getLog().error("Could not find " + artifactFile.getAbsolutePath() + " - skipping attachment");
+            }
+            projectHelper.attachArtifact(project, type, classifier, artifactFile);
+        }
+
+        try
+        {
+            reader.close();
+        }
+        catch (IOException e)
+        {
+            throw new MojoExecutionException("Could not close input file", e);
+        }
+    }
+}
