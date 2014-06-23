2a0d1482415254fc458c12b18ee5f793eb7e9903
Skip Breidbach <skip.breidbach@sri.com>
2014-06-23 10:51:41 -0700

    Import optimizations

--- build-helper-maven-plugin/src/main/java/com/sri/vt/majic/mojo/DependencyPropertiesMojo.java
+++ build-helper-maven-plugin/src/main/java/com/sri/vt/majic/mojo/DependencyPropertiesMojo.java
@@ -5,14 +5,13 @@ import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.*;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
-import org.apache.maven.project.MavenProjectHelper;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
-import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 
-import java.io.*;
+import java.io.File;
 import java.util.List;
 import java.util.Set;
 
